b0fb6e240ec3f567bd031e98b1dc72f5e6c6d987
Shigatsi <shigatsii@gmail.com>
2020-12-03 10:59:14 +0300

    bugfix: Submit btn

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,6 +5,7 @@ function PopupWithForm ({isOpen, onClose, ...props }) {
       <form className="popup__form" name={`edit_${props.name}`} method="POST" action="#" noValidate>
         <h2 className="popup__header">{props.title}</h2>
         {props.children}
+        <button type="submit" id={`profile-${props.name} `}className="popup__save-button">{props.submited}</button>
       </form>
     <button type="button" id={`${props.name}-close`} onClick = {onClose} className="popup__close-button"></button>
     </div>
