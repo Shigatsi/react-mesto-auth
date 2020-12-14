72a8d2bee87c437af98f4ffba4a8c6ffd7d9fd18
Shigatsi <shigatsii@gmail.com>
2020-12-14 22:24:04 +0300

    refactor: CurrentUserContext create

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import PopupWithForm from './PopupWithForm';
 import ImagePopup from './ImagePopup';
 import Footer from './Footer';
 import api from '../utils/Api';
+import {CurrentUserContex, currentUser} from '../contexts/CurrentUserContext';
+
 
 
 function App() {
@@ -50,65 +52,67 @@ function App() {
 
 
   return (
-    <div className="page">
-      <Header/>
-      <Main
-        onEditProfile = {handleEditProfileClick}
-        onAddPlace = {handleEditPlaceClick}
-        onEditAvatar = {handleEditAvatarClick}
-        onCardClick = {handleCardClick}
-      />
-      <PopupWithForm
-        name="avatar"
-        title="Обновить аватар"
-        submited = "Сохранить"
-        isOpen={isEditAvatarPopupOpen}
-        onClose = {closeAllPopups}
-      >
+    <CurrentUserContex.Provider value = {currentUser}>
+      <div className="page">
+        <Header/>
+        <Main
+          onEditProfile = {handleEditProfileClick}
+          onAddPlace = {handleEditPlaceClick}
+          onEditAvatar = {handleEditAvatarClick}
+          onCardClick = {handleCardClick}
+        />
+        <PopupWithForm
+          name="avatar"
+          title="Обновить аватар"
+          submited = "Сохранить"
+          isOpen={isEditAvatarPopupOpen}
+          onClose = {closeAllPopups}
+        >
 
-        <input type="url" id="popup_link" name="avatar_url" required  placeholder="Ссылка на аватар" className="popup__input"/>
-        <span className='popup__input-error' id='popup_link-input-error'></span>
-      </PopupWithForm>
+          <input type="url" id="popup_link" name="avatar_url" required  placeholder="Ссылка на аватар" className="popup__input"/>
+          <span className='popup__input-error' id='popup_link-input-error'></span>
+        </PopupWithForm>
 
-      <PopupWithForm
-        name="profile"
-        title="Редактировать профиль"
-        submited = "Сохранить"
-        isOpen={isEditProfilePopupOpen}
-        onClose = {closeAllPopups}
-      >
-        <input type="text" id="popup_name" name="name" minLength="2" maxLength="40" pattern="[а-яёА-ЯЁA-Za-z \-]*" required placeholder="Имя, Фамилия" className="popup__input"/>
-        <span className='popup__input-error popup__input-error_hidden' id='popup_name-input-error'></span>
-        <input type="text" id="popup_lifestyle" name="about" minLength="2" maxLength="200" required placeholder="Род занятий/хобби" className="popup__input"/>
-        <span className='popup__input-error popup__input-error_hidden' id='popup_lifestyle-input-error'></span>
-      </PopupWithForm>
+        <PopupWithForm
+          name="profile"
+          title="Редактировать профиль"
+          submited = "Сохранить"
+          isOpen={isEditProfilePopupOpen}
+          onClose = {closeAllPopups}
+        >
+          <input type="text" id="popup_name" name="name" minLength="2" maxLength="40" pattern="[а-яёА-ЯЁA-Za-z \-]*" required placeholder="Имя, Фамилия" className="popup__input"/>
+          <span className='popup__input-error popup__input-error_hidden' id='popup_name-input-error'></span>
+          <input type="text" id="popup_lifestyle" name="about" minLength="2" maxLength="200" required placeholder="Род занятий/хобби" className="popup__input"/>
+          <span className='popup__input-error popup__input-error_hidden' id='popup_lifestyle-input-error'></span>
+        </PopupWithForm>
 
-      <PopupWithForm
-        name="place"
-        title="Новое место"
-        submited = "Сохранить"
-        isOpen={isAddPlacePopupOpen}
-        onClose = {closeAllPopups}
-      >
-        <input type="text" id="popup_place" name="place" minLength="1" maxLength="30" required placeholder="Название" className="popup__input"/>
-        <span className='popup__input-error popup__input-error_hidden' id='popup_place-input-error'></span>
-        <input type="url" id="popup_link"name="place_url" required placeholder="Ссылка на картинку" className="popup__input"/>
-        <span className='popup__input-error' id='popup_link-input-error'></span>
-      </PopupWithForm>
+        <PopupWithForm
+          name="place"
+          title="Новое место"
+          submited = "Сохранить"
+          isOpen={isAddPlacePopupOpen}
+          onClose = {closeAllPopups}
+        >
+          <input type="text" id="popup_place" name="place" minLength="1" maxLength="30" required placeholder="Название" className="popup__input"/>
+          <span className='popup__input-error popup__input-error_hidden' id='popup_place-input-error'></span>
+          <input type="url" id="popup_link"name="place_url" required placeholder="Ссылка на картинку" className="popup__input"/>
+          <span className='popup__input-error' id='popup_link-input-error'></span>
+        </PopupWithForm>
 
-      <PopupWithForm
-        name = "confirm"
-        title="Вы уверены?"
-        submited = "Да"
-        isOpen={false}
-        onClose = {closeAllPopups}
-      >
-      </PopupWithForm>
+        <PopupWithForm
+          name = "confirm"
+          title="Вы уверены?"
+          submited = "Да"
+          isOpen={false}
+          onClose = {closeAllPopups}
+        >
+        </PopupWithForm>
 
-      <ImagePopup onCardClick={handleCardClick} card = {selectedCard || ''} onClose = {closeAllPopups}/> */
+        <ImagePopup onCardClick={handleCardClick} card = {selectedCard || ''} onClose = {closeAllPopups}/> */
 
-      <Footer/>
-  </div>
+        <Footer/>
+    </div>
+  </CurrentUserContex.Provider>
 
 
   );
